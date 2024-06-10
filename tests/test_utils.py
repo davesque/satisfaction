05dc354d287660477157788c818d09e9be77bc66
David Sanders <davesque@gmail.com>
2024-06-09 17:50:45 -0700

    Clean up SlotClass tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,14 +6,6 @@ import pytest
 from satellite import utils
 
 
-class TestClass:
-    pass
-
-
-def test_func() -> None:
-    pass
-
-
 class OneSlot(utils.SlotClass):
     __slots__ = ("foo",)
 
@@ -40,66 +32,68 @@ class Nested(utils.SlotClass):
     nested: TwoSlot
 
 
-def test_slot_class() -> None:
-    for one in (OneSlot(1), OneSlot(foo=1)):
-        assert one.foo == 1
-        with pytest.raises(AttributeError):
-            one.__dict__
+class TestSlotClass:
+    def test_class(self) -> None:
+        for one in (OneSlot(1), OneSlot(foo=1)):
+            assert one.foo == 1
+            with pytest.raises(AttributeError):
+                one.__dict__
 
-    for two in (TwoSlot(1, 2), TwoSlot(1, bar=2), TwoSlot(foo=1, bar=2)):
-        assert two.foo == 1
-        assert two.bar == 2
-        with pytest.raises(AttributeError):
-            two.__dict__
-
-
-@pytest.mark.parametrize(
-    "should_throw_error",
-    (
-        lambda: OneSlot(),
-        lambda: TwoSlot(),
-        lambda: TwoSlot(1),
-        lambda: TwoSlot(foo=1),
-        lambda: TwoSlot(bar=1),
-    ),
-)
-def test_slot_class_raises(should_throw_error: Callable[..., Any]) -> None:
-    with pytest.raises(TypeError, match="requires a setting for"):
-        should_throw_error()
+        for two in (TwoSlot(1, 2), TwoSlot(1, bar=2), TwoSlot(foo=1, bar=2)):
+            assert two.foo == 1
+            assert two.bar == 2
+            with pytest.raises(AttributeError):
+                two.__dict__
 
 
-def test_slot_class_keys() -> None:
-    assert tuple(Inherited.__keys__()) == ("foo", "bar", "bing")
-
-
-def test_slot_class_values() -> None:
-    inherited = Inherited(1, 2, 3)
-    assert tuple(inherited.__values__()) == (1, 2, 3)
-
-
-def test_slot_class_repr() -> None:
-    nested = Nested("val", TwoSlot("foo", "bar"))
-
-    assert (
-        repr(nested)
-        == dedent(
-            """
-        Nested(
-          'val',
-          TwoSlot(
-            'foo',
-            'bar',
-          ),
-        )
-        """
-        )[1:-1]
+    @pytest.mark.parametrize(
+        "should_throw_error",
+        (
+            lambda: OneSlot(),
+            lambda: TwoSlot(),
+            lambda: TwoSlot(1),
+            lambda: TwoSlot(foo=1),
+            lambda: TwoSlot(bar=1),
+        ),
     )
+    def test_class_raises(self, should_throw_error: Callable[..., Any]) -> None:
+        with pytest.raises(TypeError, match="requires a setting for"):
+            should_throw_error()
 
 
-def test_slot_class_eq() -> None:
-    nested1 = Nested("val", TwoSlot("foo", "bar"))
-    nested2 = Nested("val", TwoSlot("foo", "bing"))
-    nested3 = Nested("val", TwoSlot("foo", "bar"))
+    def test_class_keys(self) -> None:
+        assert tuple(Inherited.__keys__()) == ("foo", "bar", "bing")
 
-    assert nested1 != nested2
-    assert nested1 == nested3
+
+    def test_class_values(self) -> None:
+        inherited = Inherited(1, 2, 3)
+        assert tuple(inherited.__values__()) == (1, 2, 3)
+
+
+    def test_class_repr(self) -> None:
+        nested = Nested("val", TwoSlot("foo", "bar"))
+
+        assert (
+            repr(nested)
+            == dedent(
+                """
+            Nested(
+              'val',
+              TwoSlot(
+                'foo',
+                'bar',
+              ),
+            )
+            """
+            )[1:-1]
+        )
+
+
+    def test_class_eq(self) -> None:
+        nested1 = Nested("val", TwoSlot("foo", "bar"))
+        nested2 = Nested("val", TwoSlot("foo", "bing"))
+        nested3 = Nested("val", TwoSlot("foo", "bar"))
+
+        assert nested1 != nested2
+        assert nested1 == nested3
+        assert nested1 != 1
