93fd39bb7ef5792b8fe885637cec51ab0551c52a
David Sanders <davesque@gmail.com>
2024-06-11 20:51:31 -0700

    Fix test_queens

---
+++ satellite/examples/queens.py
@@ -0,0 +1,93 @@
+from typing import Iterator, Tuple
+
+from satellite.expr import And, Or, Var
+from satellite.utils import letters
+
+
+def partitions(vars: Tuple[Var, ...]) -> Iterator[Tuple[Var, Tuple[Var, ...]]]:
+    for i in range(len(vars)):
+        var = vars[i]
+        others = tuple(vars[:i]) + tuple(vars[i + 1 :])
+
+        yield var, others
+
+
+def exactly_one(vars: Tuple[Var, ...]) -> Or:
+    clauses = []
+    for var, others in partitions(vars):
+        no_others = ~Or(*others) if len(others) > 1 else ~others[0]
+        clauses.append(var & no_others)
+
+    return Or(*clauses)
+
+
+class Queens:
+    def __init__(self, n: int):
+        self.n = n
+        self.d = 2 * n - 1
+
+        vars = []
+        for r in range(1, n + 1):
+            for c, _ in zip(letters(), range(n)):
+                vars.append(Var(f"{c}{r}"))
+        self.vars = vars
+
+    def __getitem__(self, coords: Tuple[int, int]) -> Var:
+        c, r = coords
+        if not (0 <= c < self.n) or not (0 <= r < self.n):
+            raise IndexError(f"index is not on board: {c}, {r}")
+
+        return self.vars[r * self.n + c]
+
+    def row(self, i: int) -> Tuple[Var, ...]:
+        return tuple(self[j, i] for j in range(self.n))
+
+    def col(self, i: int) -> Tuple[Var, ...]:
+        return tuple(self[i, j] for j in range(self.n))
+
+    def ldiag(self, i: int) -> Tuple[Var, ...]:
+        diag = []
+        for j in range(self.d):
+            try:
+                diag.append(self[j + i, j])
+            except IndexError:
+                pass
+        return tuple(diag)
+
+    def rdiag(self, i: int) -> Tuple[Var, ...]:
+        diag = []
+        for j in range(self.d):
+            try:
+                diag.append(self[j + i, self.n - 1 - j])
+            except IndexError:
+                pass
+        return tuple(diag)
+
+    def get_formula(self) -> And:
+        rows = []
+        for i in range(self.n):
+            rows.append(exactly_one(self.row(i)))
+        one_per_row = And(*rows)
+
+        cols = []
+        for i in range(self.n):
+            cols.append(exactly_one(self.col(i)))
+        one_per_col = And(*cols)
+
+        ldiag = []
+        for i in range(1 - self.n, self.n):
+            diag = self.ldiag(i)
+            if len(diag) < 2:
+                continue
+            ldiag.append(exactly_one(diag) | ~Or(*diag))
+        at_most_one_per_ldiag = And(*ldiag)
+
+        rdiag = []
+        for i in range(1 - self.n, self.n):
+            diag = self.rdiag(i)
+            if len(diag) < 2:
+                continue
+            rdiag.append(exactly_one(diag) | ~Or(*diag))
+        at_most_one_per_rdiag = And(*rdiag)
+
+        return one_per_row & one_per_col & at_most_one_per_ldiag & at_most_one_per_rdiag
--- tests/test_dpll.py
+++ tests/test_dpll.py
@@ -1,6 +1,3 @@
-from itertools import combinations
-from typing import Iterator, Tuple, Iterable, Any
-
 import pytest
 
 from satellite.dpll import (
@@ -10,104 +7,11 @@ from satellite.dpll import (
     pure_literal_assign,
     unit_propagate,
 )
-from satellite.expr import And, Or, Var, var
+from satellite.examples.queens import Queens
+from satellite.expr import And, Or, var
 from satellite.tseitin import Tseitin
-from satellite.utils import numbered_var
 
 w, x, y, z = var("w x y z")
-
-
-def chunks(iter: Iterable[Any], n: int) -> Iterator[Tuple[Var, ...]]:
-    chunk = []
-    for el in iter:
-        chunk.append(el)
-        if len(chunk) == n:
-            yield tuple(chunk)
-            chunk = []
-
-    if len(chunk) > 0:
-        yield tuple(chunk)
-
-
-def partitions(vars: Tuple[Var, ...]) -> Iterator[Tuple[Var, Tuple[Var, ...]]]:
-    for i in range(len(vars)):
-        var = vars[i]
-        others = tuple(vars[:i]) + tuple(vars[i+1:])
-
-        yield var, others
-
-
-def make_vars(name1: str, n1: int, name2: str, n2: int) -> Iterator[Var]:
-    for i in range(n1):
-        for j in range(n2):
-            yield Var(f"{name1}{i}_{name2}{j}")
-
-
-def get_queen_clauses(n: int, name: str, m: int) -> Iterator[Or]:
-    var_group = make_vars(name, m, "queen", n)
-    for chunk in chunks(var_group, n):
-        for x, y in combinations(chunk, 2):
-            yield (~x | ~y)
-
-
-def letters(n: int) -> Iterator[str]:
-    a = ord("a")
-    for i in range(n):
-        yield chr(a + i)
-
-
-class Board:
-    def __init__(self, n: int):
-        self.n = n
-        self.d = 2 * n - 1
-        self.vars = [Var(f"{c}{r}") for r in range(1, n + 1) for c in letters(n)]
-
-    def __getitem__(self, coords: Tuple[int, int]) -> Var:
-        c, r = coords
-        if not (0 <= c < self.n) or not (0 <= r < self.n):
-            raise IndexError(f"index is not on board: {c}, {r}")
-
-        return self.vars[r * self.n + c]
-
-    def row(self, i: int) -> Tuple[Var, ...]:
-        return tuple(self[j, i] for j in range(self.n))
-
-    def col(self, i: int) -> Tuple[Var, ...]:
-        return tuple(self[i, j] for j in range(self.n))
-
-    def ldiag(self, i: int) -> Tuple[Var, ...]:
-        diag = []
-        for j in range(self.d):
-            try:
-                diag.append(self[j + i, j])
-            except IndexError:
-                pass
-        return tuple(diag)
-
-    def rdiag(self, i: int) -> Tuple[Var, ...]:
-        diag = []
-        for j in range(self.d):
-            try:
-                diag.append(self[j + i, self.n - 1 - j])
-            except IndexError:
-                pass
-        return tuple(diag)
-
-
-def get_queen_formula(n: int) -> And:
-    d = 2 * n - 1
-
-    clauses = []
-    for clause in get_queen_clauses(n, "row", n):
-        clauses.append(clause)
-    for clause in get_queen_clauses(n, "col", n):
-        clauses.append(clause)
-    for clause in get_queen_clauses(n, "ldiag", d):
-        clauses.append(clause)
-    for clause in get_queen_clauses(n, "rdiag", d):
-        clauses.append(clause)
-
-    return And(*clauses)
 
 
 def test_find_unit() -> None:
@@ -161,60 +65,11 @@ def test_dpll_unsat(and_expr: And) -> None:
 
 
 def test_queens() -> None:
-    board = Board(2)
+    queens = Queens(2)
 
-    rows = []
-    for i in range(board.n):
-        clauses = []
-        for var, others in partitions(board.row(i)):
-            rhs = Or(*others) if len(others) > 1 else others[0]
-            clauses.append(var & ~rhs)
-        rows.append(Or(*clauses))
+    queens_formula = queens.get_formula()
 
-    cols = []
-    for i in range(board.n):
-        clauses = []
-        for var, others in partitions(board.col(i)):
-            rhs = Or(*others) if len(others) > 1 else others[0]
-            clauses.append(var & ~rhs)
-        cols.append(Or(*clauses))
+    tseitin = Tseitin(queens_formula)
+    queens_cnf = tseitin.transform()
 
-    ldiag = []
-    for i in range(1 - board.n, board.n):
-        diag = board.ldiag(i)
-        if len(diag) < 2:
-            continue
-
-        clauses = []
-        for var, others in partitions(diag):
-            rhs = Or(*others) if len(others) > 1 else others[0]
-            clauses.append(var & ~rhs)
-
-        clauses.append(~Or(*board.ldiag(i)))
-        ldiag.append(Or(*clauses))
-
-    rdiag = []
-    for i in range(1 - board.n, board.n):
-        diag = board.rdiag(i)
-        if len(diag) < 2:
-            continue
-
-        clauses = []
-        for var, others in partitions(diag):
-            rhs = Or(*others) if len(others) > 1 else others[0]
-            clauses.append(var & ~rhs)
-
-        clauses.append(~Or(*board.rdiag(i)))
-        rdiag.append(Or(*clauses))
-
-    rows_and = And(*rows)
-    cols_and = And(*cols)
-    ldiag_and = And(*ldiag)
-    rdiag_and = And(*rdiag)
-
-    and_expr = rows_and & cols_and & ldiag_and & rdiag_and
-
-    tseitin = Tseitin(and_expr)
-    final = tseitin.transform()
-
-    assert (final, dpll(final)) == []
+    assert not dpll(queens_cnf)
